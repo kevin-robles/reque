5bcdf8451ad80ecf429410cc3dd7c3d20f04d8a5
Kevin <krobles48@outlook.com>
2019-10-21 22:53:42 -0600

    Cambio

--- reque.py
+++ reque.py
@@ -3,31 +3,33 @@ from bs4 import BeautifulSoup as soup
 import re
 
 def determinarFestividades(anno, pais) :
-    my_url = "http://www.cuandoenelmundo.com/calendario/"+pais+"/"+anno
-    try:
-        uClient = uReq(my_url)    
-        page_html = uClient.read()
-        uClient.close()
+    if isinstance(anno,int) and isinstance(pais,str):
+        my_url = "http://www.cuandoenelmundo.com/calendario/"+pais+"/"+str(anno)
+        try:
+            uClient = uReq(my_url)    
+            page_html = uClient.read()
+            uClient.close()
 
-        page_soup = soup(page_html,"html.parser")
-        hdays = page_soup.find('div',{'class':"hdays"})
-        table = hdays.find_all('table',{'class':"hdays"})
+            page_soup = soup(page_html,"html.parser")
+            hdays = page_soup.find('div',{'class':"hdays"})
+            table = hdays.find_all('table',{'class':"hdays"})
 
-        for element in table:  
-            dia = element.find_all('td')
-            dia = re.sub('<[^>]*>', '', str(dia))
-            dia = dia.replace("[","")
-            dia = dia.replace("]","")
-            final = dia.split(',')
-            i = 0
-            while i < len(final):
-                print(final[i],final[i+1],final[i+2])
-                i=i+3
-                while i > len(final):
-                    i = i-1
-                
-    except:
-        print("País o Año no disponible")
+            for element in table:  
+                dia = element.find_all('td')
+                dia = re.sub('<[^>]*>', '', str(dia))
+                dia = dia.replace("[","")
+                dia = dia.replace("]","")
+                final = dia.split(',')
+                i = 0
+                while i < len(final):
+                    print(final[i],final[i+1],final[i+2])
+                    i=i+3
+                    while i > len(final):
+                        i = i-1
+                    
+        except:
+            print("País o Año no disponible")
+    print("Tipo de datos inválidos")
 
 
 
