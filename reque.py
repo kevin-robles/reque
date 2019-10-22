ac8d0615523b29cb12752ff1496bf29544b013c6
Kevin <krobles48@outlook.com>
2019-10-21 22:45:56 -0600

    Primer función
    Lista

--- reque.py
+++ reque.py
@@ -3,19 +3,35 @@ from bs4 import BeautifulSoup as soup
 import re
 
 def determinarFestividades(anno, pais) :
-    my_url = "http://www.cuandoenelmundo.com/calendario/estados-unidos/2019"
-    uClient = uReq(my_url)    
-    page_html = uClient.read()
-    uClient.close()
+    my_url = "http://www.cuandoenelmundo.com/calendario/"+pais+"/"+anno
+    try:
+        uClient = uReq(my_url)    
+        page_html = uClient.read()
+        uClient.close()
 
-    page_soup = soup(page_html,"html.parser")
-    hdays = page_soup.find('div',{'class':"hdays"})
-    #table = hdays.find_all('table',{'class':"hdays"})
-    
-    for element in hdays:  
-        dia = element.find_all('table')  #Busca los "td" que es la información necesaria
-        tabl = dia[0].text.strip()
-        #dia = re.sub('<[^>]*>', '', str(dia)) #Elimina toda la información innecesaria
-        print(tabl)
-        print(" ")
+        page_soup = soup(page_html,"html.parser")
+        hdays = page_soup.find('div',{'class':"hdays"})
+        table = hdays.find_all('table',{'class':"hdays"})
+
+        for element in table:  
+            dia = element.find_all('td')
+            dia = re.sub('<[^>]*>', '', str(dia))
+            dia = dia.replace("[","")
+            dia = dia.replace("]","")
+            final = dia.split(',')
+            i = 0
+            while i < len(final):
+                print(final[i],final[i+1],final[i+2])
+                i=i+3
+                while i > len(final):
+                    i = i-1
+                
+    except:
+        print("País o Año no disponible")
+
+
+
+        
+   
+
 
